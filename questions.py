933c6a802caa1a9478b833b5f2e802273d49cc7d
Esteeven Andres <gallegoses2000@gmail.com>
2025-03-24 17:22:25 -0300

    Corregir uso de indices

--- questions.py
+++ questions.py
@@ -25,13 +25,18 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1];
 
 puntaje = 0;
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
+
+# El usuario deberá contestar 3 preguntas, question_to_ask creo una lista que contiene a todas
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3);
+
+
+#k hace que se seleccionen 3 valores de las que estan conformadas sus listas internas
+
+for question, options, correct_answer in questions_to_ask:
+    
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index]);
-    for i, answer in enumerate(answers[question_index]):
+    print(question);
+    for i, answer in enumerate(options):
         print(f"{i + 1}. {answer}");
        
 # El usuario tiene 2 intentos para responder correctamente
@@ -51,7 +56,7 @@ for _ in range(3):
             
         
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer:
             puntaje += 1;
             print("¡Correcto!");
             break;
@@ -61,6 +66,6 @@ for _ in range(3):
     # Si el usuario no responde correctamente después de 2 intentos,
     # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:");
-        print(answers[question_index][correct_answers_index[question_index]]);
+        print(options[correct_answer]);
 # Se imprime un blanco al final de la pregunta
-print("Obtuvo un puntaje de: ",puntaje);
+print("Obtuvo un puntaje de:",puntaje);
