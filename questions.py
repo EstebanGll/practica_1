961da1e2bf9d2fcf981833e30c6cd418af448490
Esteeven Andres <gallegoses2000@gmail.com>
2025-03-24 16:59:42 -0300

    Funcionalidad de conteo del puntaje

--- questions.py
+++ questions.py
@@ -24,6 +24,7 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1];
 
+puntaje = 0;
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -51,12 +52,15 @@ for _ in range(3):
         
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
+            puntaje += 1;
             print("¡Correcto!");
             break;
+        else:
+            puntaje -= 0.5;
     else:
     # Si el usuario no responde correctamente después de 2 intentos,
     # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:");
         print(answers[question_index][correct_answers_index[question_index]]);
 # Se imprime un blanco al final de la pregunta
-print();
+print("Obtuvo un puntaje de: ",puntaje);
